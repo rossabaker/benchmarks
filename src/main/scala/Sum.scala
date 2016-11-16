90f8eae567a2b25a648075dbac9bd28175748107
Ross A. Baker <ross@rossabaker.com>
2016-11-16 00:10:44 -0500

    Sum streams sourced from sequences

--- src/main/scala/Sum.scala
+++ src/main/scala/Sum.scala
@@ -13,7 +13,7 @@ class Sum extends BenchmarkUtils {
   var size: Int = _
 
   @Benchmark
-  def fs2: Long = {
+  def fs2Iterate: Long = {
     import _root_.fs2._
     Stream.iterate(0L)(_ + 1L)
       .take(size)
@@ -25,7 +25,18 @@ class Sum extends BenchmarkUtils {
   }
 
   @Benchmark
-  def scalazStream: Long = {
+  def fs2Chunked: Long = {
+    import _root_.fs2._
+    Stream.chunk(Chunk.seq(0 until size))
+      .sum
+      .covary[Task]
+      .runLast
+      .unsafeRun
+      .get
+  }
+
+  @Benchmark
+  def scalazStreamIterate: Long = {
     import scalaz.stream._
     Process.iterate(0L)(_ + 1L)
       .take(size)
@@ -37,7 +48,18 @@ class Sum extends BenchmarkUtils {
   }
 
   @Benchmark
-  def monixObservable: Long = {
+  def scalazStreamEmitAll: Long = {
+    import scalaz.stream._
+    Process.emitAll(0L until size)
+      .sum
+      .toSource
+      .runLast
+      .unsafePerformSync
+      .get
+  }
+
+  @Benchmark
+  def monixObservableFromStateAction: Long = {
     import scala.concurrent._, duration._
     import monix.eval._
     import monix.execution.Scheduler.Implicits.global
@@ -45,6 +67,21 @@ class Sum extends BenchmarkUtils {
     Await.result(
       Observable.fromStateAction[Long, Long](l => (l, l + 1L))(0L)
         .take(size)
+        .sumL
+        .runAsync(monix.execution.Scheduler.Implicits.global),
+      Duration.Inf
+    )
+  }
+
+
+  @Benchmark
+  def monixObservableFromIterable: Long = {
+    import scala.concurrent._, duration._
+    import monix.eval._
+    import monix.execution.Scheduler.Implicits.global
+    import monix.reactive.Observable
+    Await.result(
+      Observable.fromIterable(0L until size)
         .sumL
         .runAsync(monix.execution.Scheduler.Implicits.global),
       Duration.Inf
