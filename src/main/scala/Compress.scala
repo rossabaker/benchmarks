cc349f24523678d2e36ee6525a3f0864183bfecd
Ross A. Baker <ross@rossabaker.com>
2016-11-18 10:30:21 -0500

    Deflate only

--- src/main/scala/Compress.scala
+++ src/main/scala/Compress.scala
@@ -22,6 +22,17 @@ class Compress extends BenchmarkUtils {
   }
 
   @Benchmark
+  def fs2Gzip(): Unit = {
+    import _root_.fs2._, Stream._
+    import java.nio.file.Paths
+    io.file.readAll[Task](Paths.get("testdata/lorem-ipsum.txt"), 4096)
+      .through(compress.deflate(nowrap = true))
+      .to(io.file.writeAll[Task](Paths.get("out/lorem-ipsum.txt.gz")))
+      .run
+      .unsafeRun
+  }
+
+  @Benchmark
   def scalazStreamGzipGunzip(): Unit = {
     import _root_.scalaz.stream._, Process._
     constant(4096)
@@ -29,6 +40,17 @@ class Compress extends BenchmarkUtils {
       .pipe(compress.deflate(nowrap = true))
       .pipe(compress.inflate(nowrap = true))
       .to(nio.file.chunkW("out/lorem-ipsum.txt"))
+      .run
+      .unsafePerformSync
+  }
+
+  @Benchmark
+  def scalazStreamGzip(): Unit = {
+    import _root_.scalaz.stream._, Process._
+    constant(4096)
+      .through(nio.file.chunkR("testdata/lorem-ipsum.txt"))
+      .pipe(compress.deflate(nowrap = true))
+      .to(nio.file.chunkW("out/lorem-ipsum.txt.gz"))
       .run
       .unsafePerformSync
   }
