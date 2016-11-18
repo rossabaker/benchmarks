8b61e493ebcf771070a6c76c71fe5a6ceb9fd3a5
Ross A. Baker <ross@rossabaker.com>
2016-11-18 11:16:47 -0500

    In memory version

--- src/main/scala/Compress.scala
+++ src/main/scala/Compress.scala
@@ -9,6 +9,18 @@ import org.openjdk.jmh.annotations._
 @Warmup(iterations = 10)
 @Threads(1)
 class Compress extends BenchmarkUtils {
+  val LoremIpsum: Array[Byte] =
+    scala.io.Source.fromFile("testdata/lorem-ipsum.txt")
+      .getLines
+      .mkString("\n")
+      .getBytes
+
+  val LoremIpsumStream: fs2.Stream[fs2.Task, Byte] =
+    fs2.Stream.chunk(fs2.Chunk.bytes(LoremIpsum))
+
+  val LoremIpsumProcess: scalaz.stream.Process[scalaz.concurrent.Task, scodec.bits.ByteVector] =
+    scalaz.stream.Process.emit(scodec.bits.ByteVector.view(LoremIpsum))
+
   @Benchmark
   def fs2GzipGunzip(): Unit = {
     import _root_.fs2._, Stream._
@@ -33,6 +45,16 @@ class Compress extends BenchmarkUtils {
   }
 
   @Benchmark
+  def fs2NoIo() = {
+    import _root_.fs2._, Stream._
+    LoremIpsumStream
+      .through(compress.deflate(nowrap = true))
+      .through(compress.inflate(nowrap = true))
+      .run
+      .unsafeRun
+  }
+
+  @Benchmark
   def scalazStreamGzipGunzip(): Unit = {
     import _root_.scalaz.stream._, Process._
     constant(4096)
@@ -51,6 +73,16 @@ class Compress extends BenchmarkUtils {
       .through(nio.file.chunkR("testdata/lorem-ipsum.txt"))
       .pipe(compress.deflate(nowrap = true))
       .to(nio.file.chunkW("out/lorem-ipsum.txt.gz"))
+      .run
+      .unsafePerformSync
+  }
+
+  @Benchmark
+  def scalazStreamNoIo() = {
+    import _root_.scalaz.stream._, Process._
+    LoremIpsumProcess
+      .pipe(compress.deflate(nowrap = true))
+      .pipe(compress.inflate(nowrap = true))
       .run
       .unsafePerformSync
   }
